16c00a35b5b80176a5872786c877248173e84c85
Gustavo Solorzano Oseguera <gustavoaso26@gmail.com>
2026-05-11 21:53:49 -0600

    FEAT: Initial commit

---
+++ Models/Producto.cs
@@ -0,0 +1,13 @@
+namespace Models
+{
+    public class Producto
+    {
+        public int Id { get; set; } // 1
+        public string? Title { get; set; } // "Black clover"
+        public string? ImageId { get; set; } // 1
+        public string? Type { get; set; } // "Pin", "Sticker", "Póster"
+        public string? Category { get; set; } // "Anime", "Videojuegos", "Peliculas"
+        public decimal Price { get; set; } // 19.99
+        public int Stock { get; set; } // 10 
+    }
+}
